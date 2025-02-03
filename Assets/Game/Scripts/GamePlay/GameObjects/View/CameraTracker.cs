cca7b16f2220ecdc92e9dbd6e09aff1c7da7d519
Vladislav Melnik <GimliVlad@list.ru>
2025-02-03 16:51:12 +0300

    Add tile pallete

---
+++ Assets/Game/Scripts/GamePlay/GameObjects/View/CameraTracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Game.View
+{
+    public class CameraTracker : MonoBehaviour
+    {
+        [SerializeField] private float _minX, _maxX;
+        [SerializeField] private float _minY, _maxY;
+        [SerializeField] private Vector3 _offset;
+
+        [SerializeField] private Transform _target;
+
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void LateUpdate()
+        {
+            var position = _target.position + _offset;
+
+            position.x = Mathf.Clamp(position.x, _minX, _maxX);
+            position.y = Mathf.Clamp(position.y, _minY, _maxY);
+
+            _transform.position = position;
+        }
+    }
+}
