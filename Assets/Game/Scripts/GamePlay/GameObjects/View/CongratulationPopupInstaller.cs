25e47e89a7ca6c8fcb4983b117b67ca8c7665179
Vladislav Melnik <GimliVlad@list.ru>
2025-02-11 13:48:27 +0300

    Add end game popup

---
+++ Assets/Game/Scripts/GamePlay/GameObjects/View/CongratulationPopupInstaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.View
+{
+    public class CongratulationPopupInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject _congratulationPopup;
+
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<СongratulationPopup>()
+                .AsSingle()
+                .WithArguments(_congratulationPopup)
+                .NonLazy();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/GamePlay/GameObjects/View/СongratulationPopup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Game.Core;
+using UnityEngine;
+using Zenject;
+
+namespace Game.View
+{
+    public class СongratulationPopup : IInitializable, IDisposable
+    {
+        private readonly ILevelProgressTracker _progressTracker;
+        private readonly GameObject _popup;
+
+        public СongratulationPopup(ILevelProgressTracker progressTracker, GameObject popup)
+        {
+            _progressTracker = progressTracker;
+            _popup = popup;
+        }
+
+        public void Initialize()
+        {
+            _progressTracker.LevelCompleted += OnLevelCompleted;
+        }
+
+        public void Dispose()
+        {
+            _progressTracker.LevelCompleted -= OnLevelCompleted;
+        }
+
+        private void OnLevelCompleted()
+        {
+            _popup.SetActive(true);
+        }
+    }
+}
