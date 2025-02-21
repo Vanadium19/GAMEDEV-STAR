5de85280bd82e876794541dd0a21f04bc270ece2
Vladislav Melnik <GimliVlad@list.ru>
2025-02-21 10:04:41 +0300

    Add FSM

---
+++ Assets/Game/Scripts/Modules/FSM/StateMachine/StateTransition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Game.Modules.FSM
+{
+    public sealed class StateTransition<TKey>
+    {
+        public readonly TKey From;
+        public readonly TKey To;
+
+        private readonly Func<bool> _condition;
+
+        public StateTransition(TKey from, TKey to, Func<bool> condition)
+        {
+            From = from;
+            To = to;
+
+            _condition = condition;
+        }
+
+        public bool CanPerform()
+        {
+            return _condition == null || _condition.Invoke();
+        }
+    }
+}
