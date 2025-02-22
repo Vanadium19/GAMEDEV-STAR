bfb2e6726524af231d78ecba1829193c986398f8
ButterFish2 <godotjunior04@gmail.com>
2025-02-22 13:27:41 +0300

    add collision controller (task G-147)

---
+++ Assets/Game/Scripts/Gameplay/GameSystems/CollisionController.cs
@@ -0,0 +1,28 @@
+﻿using Game.Content.Projectiles;
+using Game.Core.Components;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Scripts.Gameplay.GameSystems
+{
+    public class CollisionController : MonoBehaviour
+    {
+        private IAttacker _attacker;
+
+        [Inject]
+        private void Construct(IAttacker attacker)
+        {
+            _attacker = attacker;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.collider.TryGetComponent(out Bullet bullet))
+            {
+                _attacker.Attack();
+            }
+
+        }
+    }
+}
