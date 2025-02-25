7a587b2b5eac9737e1f61fa3ab222598a63918df
ButterFish2 <godotjunior04@gmail.com>
2025-02-25 15:51:29 +0300

    add rifle gun

---
+++ Assets/Game/Scripts/Gameplay/GameObjects/Content/Weapons/Rifle.cs
@@ -0,0 +1,50 @@
+using Game.Content.Projectiles;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Content.Weapons
+{
+    public class Rifle : IWeapon, ITickable
+    {
+        private readonly BulletSpawner _bulletSpawner;
+        private readonly Transform _shootPoint;
+
+        private readonly float _speed;
+        private readonly float _delay;
+        private readonly int _maxAmmoCount;
+        private readonly int _damage;
+        
+        private int _ammoCount;
+        private float _currentTime;
+
+        public Rifle(BulletSpawner bulletSpawner,Transform transform,float speed, float delay, int damage, int maxAmmoCount)
+        {
+            _bulletSpawner = bulletSpawner;
+            _shootPoint = transform;
+            _speed = speed;
+            _delay = delay;
+            _maxAmmoCount = maxAmmoCount;
+            _damage = damage;
+
+            _ammoCount = _maxAmmoCount;
+        }
+
+        public void Tick()
+        {
+            if (_currentTime > 0)
+                _currentTime -= Time.deltaTime;
+        }
+
+        public void Shoot()
+        {
+            if (_currentTime > 0 || _ammoCount <= 0)
+                return;
+          
+            _bulletSpawner.Spawn(_damage, _speed, _shootPoint);
+            _currentTime = _delay;
+            _ammoCount -= 1;
+            Debug.Log($"патронов осталось : {_ammoCount}");
+        }
+    }
+}
+
