1211c9a0f116d0dbe6d19c3df7f60208f092fce4
Vladislav Melnik <GimliVlad@list.ru>
2025-02-25 03:24:32 +0300

    Add health bar

--- Assets/Game/Scripts/Gameplay/GameObjects/UI/HealthView.cs
+++ Assets/Game/Scripts/Gameplay/GameObjects/UI/HealthView.cs
@@ -1,12 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.UI
 {
     public class HealthView : MonoBehaviour
     {
+        [SerializeField] private Image _healthBar;
+
         public void SetHealth(int health, int maxHealth)
         {
             Debug.Log($"Health changed to {health} / {maxHealth}");
+
+            _healthBar.fillAmount = Mathf.Clamp01(Convert.ToSingle(health) / maxHealth);
         }
     }
 }
