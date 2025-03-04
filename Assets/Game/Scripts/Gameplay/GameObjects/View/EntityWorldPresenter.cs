df161e3dd8179706ce1dc52df3d33e19a924a1e6
Vladislav Melnik <GimliVlad@list.ru>
2025-03-04 14:03:14 +0300

    Add entity count view

---
+++ Assets/Game/Scripts/Gameplay/GameObjects/View/EntityWorldPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Game.GameObjects;
+using R3;
+using Zenject;
+
+namespace Game.View
+{
+    public class EntityWorldPresenter : IInitializable, IDisposable
+    {
+        private readonly EntityWorld _entityWorld;
+        private readonly EnemiesCountView _view;
+
+        private IDisposable _disposables;
+
+        public EntityWorldPresenter(EntityWorld entityWorld, EnemiesCountView view)
+        {
+            _entityWorld = entityWorld;
+            _view = view;
+        }
+
+        public void Initialize()
+        {
+            var disposableBuilder = Disposable.CreateBuilder();
+
+            _entityWorld.CurrentEnemiesCount.Subscribe(OnEnemiesCountChanged).AddTo(ref disposableBuilder);
+
+            _disposables = disposableBuilder.Build();
+        }
+
+        public void Dispose()
+        {
+            _disposables?.Dispose();
+        }
+
+        private void OnEnemiesCountChanged(int count)
+        {
+            string text = $"Осталось врагов {count}";
+
+            _view.ShowEnemiesCount(text);
+        }
+    }
+}
--- Assets/Game/Scripts/Gameplay/GameSceneInstaller.cs
+++ Assets/Game/Scripts/Gameplay/GameSceneInstaller.cs
@@ -1,4 +1,8 @@
-﻿using Game.Content.Projectiles;
+﻿using Game.Content.Player;
+using Game.Content.Projectiles;
+using Game.Core;
+using Game.GameObjects;
+using Game.View;
 using UnityEngine;
 using Zenject;
 
@@ -6,12 +10,34 @@ namespace Game.Scripts.Gameplay
 {
     public class GameSceneInstaller : MonoInstaller
     {
-        [SerializeField] private Bullet _bulletPrefab;
+        [SerializeField] private Entity _player;
+        [SerializeField] private EnemiesCountView _enemiesCountView;
+
+        [Header("Bullets")] [SerializeField] private Bullet _bulletPrefab;
         [SerializeField] private Transform _bulletsContainer;
 
         public override void InstallBindings()
         {
+            Container.Bind<CharacterProvider>()
+                .AsSingle()
+                .WithArguments(_player);
+
+            Container.BindInterfacesAndSelfTo<EntityWorld>()
+                .AsSingle()
+                .NonLazy();
+
+            //Bullets
             BulletsInstaller.Install(Container, _bulletPrefab, _bulletsContainer);
+
+            //Presenters
+            Container.BindInterfacesTo<EntityWorldPresenter>()
+                .AsSingle()
+                .NonLazy();
+
+            //UI
+            Container.Bind<EnemiesCountView>()
+                .FromInstance(_enemiesCountView)
+                .AsSingle();
         }
     }
 }
